36f08d0a349f30bf1cf64abc40aea65bae02a12c
Amol Mane <amolmane@alumni.ubc.ca>
2016-12-19 13:10:31 +0400

    added documentation

---
+++ car.R
@@ -0,0 +1,19 @@
+#' Class Car
+#' 
+#' Stores details of car and contains method for mpg per dollar.
+
+
+#' Constructor for class 'Car' 
+#' 
+#' @param model A string
+#' @param mpg A number
+#' @param cost A number
+#' @examples
+#' car("focus", 20, 30000)
+
+
+#' Return mpg per dollar
+#' 
+#' @examples
+#' my_car <- car("focus", 20, 30000)
+#' my_car.mpg_per_dollar()
